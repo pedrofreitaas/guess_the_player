684a83236f9f844550264986522e6df41844d68a
pedrofreitaas <freitaas.pedro@gmail.com>
2023-07-20 13:34:51 -0300

    found the correct API, and made a few progress in the player show

--- scripts/script.js
+++ scripts/script.js
@@ -1,77 +1,70 @@
 import * as fb from "./firebase.js"
 
-let player_component_innerHTML_pattern = 
-`
-<div class="player_container">
-<h3 class="player_name"> ##name </h3>
-<img src="##path_to_pic" class="player_pic">
-<p class="player_info">
-	Age: ##age <br/>
-	League: ##curr_league <br/>
-	Club: ##curr_club <br/>
-	Position: ##position <br/>
-	Foot: ##foot <br/>
-</p>
-</div>
-`;
-
 async function retrieveRandomPlayerID() {
-	const players = fb.collection(fb.db, '/players');
-	
-	const playersSnapshot = await fb.getDocs(players);
-
-	const ids = playersSnapshot.docs.map(doc => doc.id)
-
-	return ids[Math.floor(Math.random() * ids.length)];
+	return Math.floor(Math.random() * 18882);
 };
 
-function fitPlayerInfoIntoHTML( playerInfo ) {
-	let player_innerHTML = player_component_innerHTML_pattern.replace("##name", playerInfo.name);
-	player_innerHTML = player_innerHTML.replace("##path_to_pic", playerInfo.path_to_pic);
-	player_innerHTML = player_innerHTML.replace("##age", playerInfo.age);
-	player_innerHTML = player_innerHTML.replace("##curr_league", playerInfo.league);
-	player_innerHTML = player_innerHTML.replace("##curr_club", playerInfo.club);
-	player_innerHTML = player_innerHTML.replace("##position", playerInfo.pos);
-	player_innerHTML = player_innerHTML.replace("##foot", playerInfo.foot);
+async function fitPlayerInfoIntoHTML( playerJSON ) {
+	const playerImgResp = await fetch(`https://futdb.app/api/players/${playerJSON.id}/image`, { 
+									  method: "GET",
+									  headers: {
+										"Accept": "*",
+										"X-Auth-Token": "29db2ef2-4419-4447-8253-99755c0f6a55",
+								      },
+									})
 
-	return player_innerHTML
+	const clubImgResp = await fetch(`https://futdb.app/api/clubs/${playerJSON.club}/image`, { 
+										method: "GET",
+										headers: {
+										  "Accept": "*",
+										  "X-Auth-Token": "29db2ef2-4419-4447-8253-99755c0f6a55",
+										},
+									  })
+
+	const leagueImgResp = await fetch(`https://futdb.app/api/leagues/${playerJSON.league}/image`, { 
+									  method: "GET",
+									  headers: {
+										"Accept": "*",
+										"X-Auth-Token": "29db2ef2-4419-4447-8253-99755c0f6a55",
+								      },
+									})
+
+	return `
+	<h3 class="player_name"> ${playerJSON.commonName} </h3>
+	<img width="90px" height="90px" src="${URL.createObjectURL(await playerImgResp.blob())}">
+	<h3> ${playerJSON.rating} </h3>
+	<p class="player_info">
+		Date of Birth: ${playerJSON.birthDate} <br/>
+		Leagues: <img width="30px" height="30px" src="${URL.createObjectURL(await leagueImgResp.blob())}"> <br/>
+		Club: <img width="30px" height="30px" src="${URL.createObjectURL(await clubImgResp.blob())}"> <br/>
+		Position: ${playerJSON.position} <br/>
+		Pace: ${playerJSON.pace} <span class="tab"> Shooting: ${playerJSON.shooting} <span class="tab"> <br/>
+		Passing: ${playerJSON.passing} <span class="tab"> Dribbling: ${playerJSON.dribbling} <span class="tab"> <br/>
+		Defending: ${playerJSON.defending} <span class="tab"> Physicality: ${playerJSON.physicality} <span class="tab"> <br/>
+	</p>
+	`
 }
 
 async function getPLayerByID( id ) {
-	const players = fb.collection(fb.db, '/players');
-	
-	const playersSnapshot = await fb.getDocs(players);
+	let response = await fetch(`https://futdb.app/api/players/${id}`, { 
+		method: "GET",
+		headers: {
+			"Accept": "*/*",
+			"X-Auth-Token": "29db2ef2-4419-4447-8253-99755c0f6a55",
+		},
+	});
 
-	const player = playersSnapshot.docs.filter( doc => doc.id==id ).map(doc => doc.data());
-
-	try{
-		return player[0];
-	} catch (RangeError) {
-		return undefined;
-	}
-}
-
-async function generatePlayer() {
-	try {
-		const docRef = await fb.addDoc( fb.collection(fb.db, "/players"), {
-
-										});
-
-		console.log(`Written player with id: ${docRef}`);
-	} catch (error) {
-		console.log(error);
-	}							
-
+	const responseJson = await response.json();
+	return responseJson.player;
 }
 
 async function createPlayerContainer( playerID ) {
 	let newPlayer = document.createElement("div");
+	newPlayer.classList.add("player_container");
 
 	const playerInfo = await getPLayerByID(playerID);
 
-	if (playerInfo === undefined) return;
-
-	newPlayer.innerHTML = fitPlayerInfoIntoHTML( playerInfo );
+	newPlayer.innerHTML = await fitPlayerInfoIntoHTML(playerInfo);
 
 	document.body.append(newPlayer);
 };
@@ -80,22 +73,4 @@ async function createRandomPlayerContainer() {
 	await createPlayerContainer( await retrieveRandomPlayerID() );
 };
 
-// await createRandomPlayerContainer();
-
-async function testAPI() {
-	let response = await fetch("https://api.football-data.org/v4/competitions/SA/scorers", { 
-		method: "GET",
-		headers: {
-			"Accept": "*/*",
-			"X-Auth-Token": "a350206d55574584b9d29d30aa5b30f2",
-		},
-		origin: "http://localhost",
-	});
-
-	console.log(response);
-
-	let data = await response.text();
-	console.log(data);
-};
-
-await testAPI();
+await createRandomPlayerContainer();
