a1998847643341435673882f79c989f4d587cd20
pedrofreitaas <freitaas.pedro@gmail.com>
2023-07-24 15:44:57 -0300

    started to hide info

--- public/script.js
+++ public/script.js
@@ -1,4 +1,39 @@
-var ableToTip = false;
+const positions = ["CF", "RWB", "MD", "CAM", "CB", "LB", "RB", "SW", 
+				   "LWB", "RWB", "CM", "AM", "DM", "LM", "RM", "CF",
+				   "S", "SS"];
+
+function getRandomIntegerFromTo(a=0, b=0) {
+	return Math.floor( Math.random() * b + a );
+}				   
+
+// card atributtes:
+function hideAtributte( atributteValue, type=null ) {
+	switch( type ) {
+		case "birthDate":
+			return atributteValue.slice(0, 4) + "-??-??";
+
+		case "rating":
+			return String(atributteValue).charAt(0) + " ?";
+
+		case "position":
+			let possiblePositions = [positions[getRandomIntegerFromTo(0, positions.length)],
+									 positions[getRandomIntegerFromTo(0, positions.length)],
+									 positions[getRandomIntegerFromTo(0, positions.length)]];
+			
+			possiblePositions[getRandomIntegerFromTo(0, 2)] = atributteValue;
+
+			return possiblePositions.join(" or ");
+
+		case "club":
+		case "league":
+		case "name":
+			return "?";
+
+		case null:
+		default:
+			return atributteValue;
+	}
+}
 
 async function retrieveRandomPlayerIDWithMinRating(minimum_rating) {	
 	while (true) {
@@ -23,12 +58,12 @@ async function getPlayerImgURLByID( id ) {
 	const playerImgResp = await fetch(`https://futdb.app/api/players/${id}/image`, { 
 		method: "GET",
 		headers: {
-	  		"Accept": "*",
+	  		"Accept": "image/png",
 	  		"X-Auth-Token": "29db2ef2-4419-4447-8253-99755c0f6a55",
 		},
 	});
 
-  	return URL.createObjectURL(await playerImgResp.blob());
+	return URL.createObjectURL(await playerImgResp.blob());
 }
 
 async function getPlayerClubImgURLByID( id ) {
@@ -68,13 +103,17 @@ async function getPlayerJSONByID( id ) {
 	responseJson = responseJson.player;
 
 	const playerJSON = {
-		name: responseJson.commonName,
+		id: responseJson.id,
+		clubID: responseJson.club,
+		leagueID: responseJson.league,
+		obtainIcon: async (id) => getPlayerImgURLByID(id),
+		obtainLeagueIcon: async (leagueID) => getPlayerLeagueImgURLBy(leagueID),
+		obtainClubIcon: async (clubID) => getPlayerClubImgURLByID(clubID),
+		
+		name: responseJson.commonName,		
 		birthDate: responseJson.birthDate,
 		rating: responseJson.rating,
 		position: responseJson.position,
-		icon: await getPlayerImgURLByID(responseJson.id),
-		leagueIcon: await getPlayerLeagueImgURLBy(responseJson.league),
-		clubIcon: await getPlayerClubImgURLByID(responseJson.club),
 		pace: responseJson.pace,
 		dribbling: responseJson.dribbling,
 		finishing: responseJson.finishing,
@@ -87,35 +126,25 @@ async function getPlayerJSONByID( id ) {
 	return playerJSON;
 }
 
-function fitPlayerJSONIntoHTML( playerJSON ) {
+async function fitPlayerJSONIntoHTML( playerJSON ) {
 	return `
-	<h1 class="player_name"> ${playerJSON.name} </h1>
-	<img width="90px" height="90px" class="player_pic" src="${playerJSON.icon}">
-	<h3 class="player_rating"> ${playerJSON.rating} </h3>
+	<h1 class="player_name"> ${hideAtributte( playerJSON.name, "name" )} </h1>
+	<img width="120px" height="120px" class="player_pic" src="${await playerJSON.obtainIcon(playerJSON.id)}">
+	<h3 class="player_rating"> ${hideAtributte( playerJSON.rating, "rating" )} </h3>
+	
 	<p class="player_info">
-		<i class="fa-solid fa-baby" style="color: #0c2145;"></i> Date of Birth: ${playerJSON.birthDate} <br/>
-		<i class="fa-solid fa-trophy" style="color: #07155a;"></i> League: <img class="icon" width="30px" height="30px" src="${playerJSON.leagueIcon}"> <br/>
-		<i class="fa-solid fa-scroll" style="color: #0f203e;"></i> Club: <img class="icon" width="30px" height="30px" src="${playerJSON.clubIcon}"> <br/>
-		<i class="fa-solid fa-futbol" style="color: #112a55;"></i> Position: ${playerJSON.position} <br/>
+		<i class="fa-solid fa-baby" style="color: #0c2145;"></i> Date of Birth: ${hideAtributte(playerJSON.birthDate, "birthDate")} <br/>
+		<i class="fa-solid fa-trophy" style="color: #07155a;"></i> League and Club: <img class="icon" width="30px" height="30px" src="${await playerJSON.obtainLeagueIcon(playerJSON.leagueID)}"> <img class="icon" width="30px" height="30px" src="${await playerJSON.obtainClubIcon(playerJSON.clubID)}"> <br/>
+		<i class="fa-solid fa-futbol" style="color: #112a55;"></i> Position: ${hideAtributte(playerJSON.position, "position")} <br/>
 	</p>
-	<div class="player_atrib_line_l"> <i class="fa-solid fa-person-running" style="color: #1e3c71;"></i> Pace: <div id="player_pace"> ${playerJSON.pace} </div> </div>
-	<div class="player_atrib_line_r"> <div id="player_shooting"> ${playerJSON.shooting} </div> Shooting: <i class="fa-solid fa-meteor" style="color: #102446;"></i> </div>
-	<div class="player_atrib_line_l"> <i class="fa-solid fa-people-arrows" style="color: #355997;"></i> Passing: <div id="player_passing"> ${playerJSON.passing} </div> </div>
-	<div class="player_atrib_line_r"> <div id="player_dribbling"> ${playerJSON.dribbling} </div> Dribbling: <i class="fa-solid fa-user-ninja" style="color: #142d57;"></i> </div>
-	<div class="player_atrib_line_l"> <i class="fa-solid fa-shield-halved" style="color: #172f59;"></i> Defending:<div id="player_defending"> ${playerJSON.defending} </div> </div>
-	<div class="player_atrib_line_r"> <div id="player_physicality"> ${playerJSON.physicality} </div> Physicality: <i class="fa-solid fa-dumbbell" style="color: #20437e;"></i> </div>
+
+	<div class="player_atrib_line_l"> <i class="fa-solid fa-person-running" style="color: #1e3c71;"></i> Pace: <i id="pace"> ${hideAtributte(playerJSON.pace, "rating")} </i> </div>
+	<div class="player_atrib_line_r"> <i class="fa-solid fa-meteor" style="color: #102446;"></i> Shooting: ${hideAtributte(playerJSON.shooting, "rating")} </div>
+	<div class="player_atrib_line_l"> <i class="fa-solid fa-people-arrows" style="color: #355997;"></i> Passing: ${hideAtributte(playerJSON.passing, "rating")} </div>
+	<div class="player_atrib_line_r"> <i class="fa-solid fa-user-ninja" style="color: #142d57;"></i> Dribbling: ${hideAtributte(playerJSON.dribbling, "rating")} </div>
+	<div class="player_atrib_line_l"> <i class="fa-solid fa-shield-halved" style="color: #172f59;"></i> Defending: ${hideAtributte(playerJSON.defending, "rating")} </div>
+	<div class="player_atrib_line_r"> <i class="fa-solid fa-dumbbell" style="color: #20437e;"></i> Physicality: ${hideAtributte(playerJSON.physicality, "rating")} </div>
 	`
-}
-
-function insertLoadIcon() {
-	const loadIcon = document.createElement("div");
-	loadIcon.classList.add("player_container_load");
-
-	document.body.getElementsByClassName("player_container")[0].appendChild(loadIcon);
-}
-
-function removeLoadIcon() {
-	document.body.getElementsByClassName("player_container")[0].removeChild( document.getElementsByClassName("player_container_load")[0] );
 }
 
 window.onload = async function choosePlayer() {
@@ -125,19 +154,24 @@ window.onload = async function choosePlayer() {
 	playerContainer.classList.add("player_container");
 	document.body.appendChild(playerContainer);
 
-	insertLoadIcon();
+	const loadIcon = document.createElement("div");
+	loadIcon.classList.add("player_container_load");
+	playerContainer.appendChild(loadIcon);
 
-	if( playerJSON != null )
-		playerContainer.innerHTML = fitPlayerJSONIntoHTML( playerJSON );
+	if( playerJSON != null ) {
+		playerJSON.obtainIcon = async (id) => getPlayerImgURLByID(id);
+		playerJSON.obtainClubIcon = async (clubID) => getPlayerClubImgURLByID(clubID);
+		playerJSON.obtainLeagueIcon = async (leagueID) => getPlayerLeagueImgURLBy(leagueID);
+		
+		playerContainer.innerHTML = await fitPlayerJSONIntoHTML( playerJSON );
+	}
 	
 	else {
 		playerJSON = await getPlayerJSONByID( await retrieveRandomPlayerIDWithMinRating(80) );
-		playerContainer.innerHTML = fitPlayerJSONIntoHTML(playerJSON);
+		playerContainer.innerHTML = await fitPlayerJSONIntoHTML(playerJSON);
 		
 		localStorage.setItem( "playerJSON", JSON.stringify(playerJSON) );
 	}
-	
-	removeLoadIcon(); 
 }
 
 export function resetPlayer() {
@@ -145,23 +179,33 @@ export function resetPlayer() {
 	location.reload();
 }
 
-// recover player Tip's timer.
-setInterval( () => {
-	ableToTip=true;
+var ableToTip = true;
+export function tryToTip() {
+
+	if ( !ableToTip )
+		return
+
+	giveTip();
 
 	const tipButton = document.getElementsByClassName("player_tip_button")[0];
-	tipButton.style.textDecoration = "none";
-}, 10000);
+	tipButton.style.textDecoration = "line-through";
 
-export function tryToTip() {
-	if (ableToTip) {
-		console.log("dando dica.");
-		ableToTip = false;
-
+	// recover tip.
+	setTimeout( () => {
+		ableToTip=true;
+	
 		const tipButton = document.getElementsByClassName("player_tip_button")[0];
-		tipButton.style.textDecoration = "line-through";
-	}
+		tipButton.style.textDecoration = "none";
+	}, 10000);
+}
 
-	else
-		console.log("sem dica");
+var current_tip = 0;
+function giveTip() {
+
+	switch (current_tip) {
+	};
+	
+	
+	ableToTip = false;
+	return 0;
 }
